3a9aa8947939d6fcde3ac40beb4259c77515e41c
huisheng.jin <huisheng.jinge@gmail.com>
2021-04-27 19:47:21 +0800

    add test demo

---
+++ src/test/java/kata/ExampleTest.java
@@ -0,0 +1,12 @@
+package kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class ExampleTest {
+    @Test
+    void should_success() {
+        assertThat(1).isEqualTo(1);
+    }
+}
